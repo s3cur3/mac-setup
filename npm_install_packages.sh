896c4a6e36e705966177331e17691480d8bcbaee
Tyler Young <tyler@x-plane.com>
2014-11-02 09:26:02 -0600

    Added NPM packages

--- npm_install_packages.sh
+++ npm_install_packages.sh
@@ -4,4 +4,15 @@ echo "#######################################################################"
 echo "NOTE: You should only run this after installing Node manually (http://nodejs.org/download/)"
 echo "#######################################################################"
 
-npm install bro
+
+packages=(
+  bro
+  grunt
+  ionic
+)
+
+# install packages
+echo "installing Node.js packages..."
+sudo npm install ${packages[@]}
+
+
